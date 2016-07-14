f6150cd3fab961eb53fe42318c89d4ab061e142f
Hem Brahmbhatt <hamant.brahmbhatt@net-a-porter.com>
2016-07-14 16:23:07 +0100

    websocket server

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ var url = require('url')
 wss.on('connection', function connection(ws) {
   var location = url.parse(ws.upgradeReq.url, true);
 
-  // you might use location.query.access_token to authenticate or share sessions
-  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
 
   // ws.on('message', function incoming(message) {
   //   console.log('received: %s', message);
   // });
   //
   // ws.send('something');
+
   // Turn one of the LEDs on to start.
   tessel.led[2].on();
 
